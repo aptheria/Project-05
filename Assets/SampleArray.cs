e57e9a068d011dec6815eb67eba7755a868ef87a
aptheria <aptheria@iu.edu>
2024-11-28 17:59:39 -0500

    Game Mostly Finished

---
+++ Assets/SampleArray.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleArray : MonoBehaviour
+{
+    public float[] samplesL = new float[1024];
+    public float[] samplesR = new float[1024];
+}
